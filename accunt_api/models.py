d4367c716f8331e5ebc91c2b1b907b20909c20e6
parisafarivashh <parisafarivash@gmail.com>
2020-03-07 09:19:57 +0200

    model add

--- accunt_api/models.py
+++ accunt_api/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+
+class MyAccountManager(BaseUserManager):
+  def create_user(self,email,username,password=None):
+    if not email:
+      raise ValueError("user must be have email")
+    if not username:
+      raise ValueError("user must be have username")
+    user =self.model(
+      email =self.normalize_email(email),
+      username = username
+    )
+    
+    user.set_password(password)
+    user.save(using = self._db)
+    return user
+
+  def create_superuser(self,email,username,password):
+    user = self.create_user(
+      email = self.normalize_email(email),
+      username =username,
+      password=password
+    )  
+    
+    user.is_admin =True
+    user.is_staff =True
+    user.is_superuser =True
+    user.save(using =self._db)
+    return user
+
+
+class Account(AbstractBaseUser):
+  email = models.EmailField(max_length=30,unique=True)
+  username = models.CharField(max_length=30)
+  time_join = models.DateTimeField(auto_now_add=True)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+  is_superuser = models.BooleanField(default=False)
+  is_admin = models.BooleanField(default=False)
+
+
+  objects = MyAccountManager()
+
+  USERNAME_FIELD ='email'
+  REQUIRED_FIELDS =['username']
+
+  def __str__(self):
+    return self.email
+
+  def has_perm(self,perm,obj=None):
+    return self.is_admin
+
+  def has_module_perms(self,app_label):
+    return True    
+
+     
--- project_api/settings.py
+++ project_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accunt_api.apps.AccuntApiConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL ='accunt_api.Account'
