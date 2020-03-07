a4cbdd41c5047486f2d445ead781715d3e5bbb7d
parisafarivashh <parisafarivash@gmail.com>
2020-03-07 11:34:50 +0200

    authtoken

--- accunt_api/views.py
+++ accunt_api/views.py
@@ -4,6 +4,7 @@ from .serializer import AccountSerializer
 from rest_framework import viewsets
 from accunt_api.models import Account
 
+
 class Account_viewset(viewsets.ModelViewSet):
   serializer_class = AccountSerializer
   queryset =Account.objects.all()
